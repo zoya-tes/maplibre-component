74437e28b20135a2fdaba47b97c3a0dc3fb747c5
zoyaiqbal03 <zoya.iqbal@targetofs.com>
2024-07-05 13:07:14 +0500

    Added antd and sass file

--- src/Map.js
+++ src/Map.js
@@ -1,6 +1,9 @@
+/* eslint-disable no-unused-vars */
+import 'maplibre-gl/dist/maplibre-gl.css'
 import React, { useState, useEffect } from 'react'
 import maplibregl from 'maplibre-gl'
-import 'maplibre-gl/dist/maplibre-gl.css'
+import { Typography } from 'antd'
+import styles from './Map.scss'
 
 const Map = () => {
   const [map, setMap] = useState(null)
@@ -15,9 +18,16 @@ const Map = () => {
     })
     setMap(mapInstance)
   }, [])
+  const { Title } = Typography
   return (
     <div>
-      <div id='map' style={{ width: '100%', height: '500px' }} />
+      <Title className={styles.heading}>Map</Title>
+      <div className={styles.title}>This is Basic map of maplibre-gl</div>
+      <div
+        className={styles.mapborder}
+        id='map'
+        style={{ width: '100%', height: '500px' }}
+      />
     </div>
   )
 }
